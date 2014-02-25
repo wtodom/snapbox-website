7928d9f2c54725d05eb6e9f9de46a8a3cdf842b5
Weston Odom <weston.odom@gmail.com>
2014-02-24 22:40:27 -0600

    added infor to the team page and some other basics

--- capstone.py
+++ capstone.py
@@ -12,6 +12,22 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/goals")
+def goals():
+	return render_template("goals.html")
+
+@app.route("/downloads")
+def downloads():
+	return render_template("downloads.html")
+
+@app.route("/team")
+def team():
+	return render_template("team.html")
+
+@app.route("/deliverables")
+def deliverables():
+	return render_template("deliverables.html")
+
 # Run it.
 if __name__ == "__main__":
 	app.run()
